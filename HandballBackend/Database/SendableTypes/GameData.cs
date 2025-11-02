ae5aa97ac56e7b93e4b4295a6538e6cfea217a04
healy <healy_jared@yahoo.com>
2025-11-03 02:31:42 +0800

    dotnet format

--- HandballBackend/Database/SendableTypes/GameData.cs
+++ HandballBackend/Database/SendableTypes/GameData.cs
@@ -95,7 +95,7 @@ public class GameData {
     public int Court { get; private set; }
 
     public bool BlitzGame { get; private set; }
-    
+
     public int ReplaysSinceScore { get; private set; }
 
     public GameData(
@@ -158,12 +158,12 @@ public class GameData {
             .OrderByDescending(gE => gE.Id).FirstOrDefault();
         FirstTeamScoredLast = game.TeamOneId == mostRecentPoint?.TeamId;
         BlitzGame = game.BlitzGame;
-        
+
         ReplaysSinceScore = game.Events.OrderByDescending(a => a.Id)
             .Where(a => a.EventType is GameEventType.Replay or GameEventType.Score)
             .TakeWhile(a => a.EventType != GameEventType.Score)
             .Count();
-        
+
         if (includeGameEvents) {
             Events = game.Events.Select(a => a.ToSendableData()).OrderBy(gE => gE.Id).ToArray();
         }
